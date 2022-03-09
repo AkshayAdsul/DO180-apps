ce3cd16179664bba1fb07c428ee14d893af4871d
AkshayAdsul <60718977+AkshayAdsul@users.noreply.github.com>
2022-03-09 20:58:46 +1100

    Update app.js
    wizdemo changes

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('You should hire Akshay! Highly recommended Wizard - Prof. Dumbledore');
 });
 
 app.listen(8080, function () {
