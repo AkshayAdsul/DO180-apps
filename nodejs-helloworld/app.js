b4559736f5751c11b064f22f81fdb4b907962541
AkshayAdsul <60718977+AkshayAdsul@users.noreply.github.com>
2022-03-11 12:06:41 +1100

    Update app.js

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -1,8 +1,56 @@
 var express = require('express');
 app = express();
 
+const mongo = require('mongodb').MongoClient;
+
+const mongoUri = process.env.uri;
+const mongoUsername = process.env.username || process.env.MONGODB_USER;
+const mongoPassword = process.env.password || process.env.MONGODB_PASSWORD;
+const dbName = process.env.database_name || 
+			   process.env.MONGODB_DBNAME || 
+			   process.env.MONGODB_DATABASE ||
+			   'sampledb';
+const dbServiceName = process.env.DATABASE_SERVICE_NAME || 'localhost';
+
+var dbConnectionUrl;
+
+// If the monogo secret has been attached, modify the provided URI to include
+// authentication credentials
+if (mongoUri) {
+	var auth = mongoUsername + ':' + mongoPassword + '@'
+	var pieces = mongoUri.split('//');
+	dbConnectionUrl = pieces[0] + '//' + auth + pieces[1] + '/' + dbName;
+}
+else if (process.env.MONGODB_URL){
+	dbConnectionUrl = process.env.MONGODB_URL || 'mongodb://localhost:27017/sampledb';
+} else {
+	dbConnectionUrl = 'mongodb://' + mongoUsername + ':' + 
+					mongoPassword + '@' + 
+					dbServiceName + ':27017/' 
+					+ dbName;
+}
+
 app.get('/', function (req, res) {
   res.send('You should hire Akshay, I highly recommend him as a Wizard!   - Prof. Dumbledore');
+});
+
+app.get('/debug', function(req, res, next) {
+
+	var details = {
+		"mongo_url": dbConnectionUrl,
+		"connected": false
+	};
+
+	mongo.connect(dbConnectionUrl, (err, client) => {
+		if (err) {
+			console.error(err)
+		} else {
+			console.log('Connected to Mongo')
+			details["connected"] = true;
+			console.log("Updated details")
+		}
+		res.send(details);
+	});
 });
 
 app.listen(8080, function () {
